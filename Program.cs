1e37fd5d1fcb4ba7e03b65f2b02444db5c7452d8
daitxsuelen <suelendaitx@yahoo.com.br>
2024-10-21 13:59:35 -0300

    "second"

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Loja loja = new Loja();
+
+    ProdutoFisico livro = new ProdutoFisico("Livro", 50, 0.5, "Livros");
+    ProdutoDigital eBook = new ProdutoDigital("Ebook", 30, 10, "PDF");
+
+    loja.CadastrarProduto(livro);
+    loja.CadastrarProduto(eBook);
+
+    //Cadastro de cliente
+    Cliente cliente = new Cliente("Suelen", "123456789", "Rua José Luiz de Freitas", "suelendaitx@rede.ulbra.br");
+    loja.CadastrarCliente(cliente);
+
+    //Criação de perido
+    Pedido pedido = loja.CriarPedido(cliente);
+    pedido.AdicionarProduto(livro);
+    pedido.AdicionarProduto(eBook);
+    
+
+    //Finalização do pedido
+
+    loja.FinalizarPedido(pedido);
+
+    }
+    
+}
--- obj/Debug/net8.0/AP2-Suelen.AssemblyInfo.cs
+++ obj/Debug/net8.0/AP2-Suelen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AP2-Suelen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+329a5327842ff0d36928524fa0428c9efa2d5911")]
 [assembly: System.Reflection.AssemblyProductAttribute("AP2-Suelen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AP2-Suelen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
